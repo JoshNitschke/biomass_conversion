5223d0f9850e2810c4da55d4aeecbd76b7a410ac
Fonti Kar <f.kar@unsw.edu.au>
2023-07-21 10:32:54 +1000

    Update data_explore.R
    Added packages at the start and functions to get overview of frozen data samples

--- R/data_explore.R
+++ R/data_explore.R
@@ -1,3 +1,19 @@
+# This is Josh's R Script
+
+# Load packages
+library(tidyverse)
+
+# install.packages("skimr")
+library(skimr)
+
+# Reading in data
 biomass_data <- read.csv("data/biomass_conversions.csv")
+
+# Filtering Frozen samples
 biomass_data_frozen <- biomass_data %>%
   filter(Preservation.Method == "Frozen")
+
+# Check out structure of biomass_data_frozen
+str(biomass_data_frozen)
+skim(biomass_data_frozen)
+
